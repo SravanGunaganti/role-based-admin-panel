a3db4908245dd40206769fc81a7081da1994097d
SravanGunaganti <gunagantisravan376@gmail.com>
2025-05-14 20:27:55 +0530

    Project initalization with basic set up

---
+++ README.md
@@ -0,0 +1,1 @@
+# **Role-Based Management Admin Panel**
---
+++ client/src/App.tsx
@@ -0,0 +1,10 @@
+
+
+function App() {
+
+  return (
+    <h1 className="text-4xl text-center" >MERN Stack Role-Based Access Control Panel</h1>
+  )
+}
+
+export default App
